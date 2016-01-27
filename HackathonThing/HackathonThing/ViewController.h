8f1fcb532538567f082ffd06fdfc61d5d95c9e22
Timothy F. Bowen <tim@berecruited.com>
2016-01-27 08:33:06 -0800

    initial commit

---
+++ HackathonThing/HackathonThing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HackathonThing
+//
+//  Created by Timothy F. Bowen on 1/27/16.
+//  Copyright © 2016 Reigning Champs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HackathonThing/HackathonThing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HackathonThing
+//
+//  Created by Timothy F. Bowen on 1/27/16.
+//  Copyright © 2016 Reigning Champs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
